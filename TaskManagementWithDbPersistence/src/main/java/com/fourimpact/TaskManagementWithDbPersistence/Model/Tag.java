c2c200ba1453b154feeeaaac03126eff66f5bb09
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-04-17 09:32:38 +0800

    Added Tag Entity

---
+++ TaskManagementWithDbPersistence/src/main/java/com/fourimpact/TaskManagementWithDbPersistence/Model/Tag.java
@@ -0,0 +1,27 @@
+package com.fourimpact.TaskManagementWithDbPersistence.Model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tag")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Task> tasks = new ArrayList<>();
+
+    public Tag() {}
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}
